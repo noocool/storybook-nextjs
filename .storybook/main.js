319a2fd7816cad780dec28b518e1fdb1ffa9cd12
noocool <ngothinh287@gmail.com>
2022-09-21 12:48:05 +0700

    Init code

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,21 @@
+// .storybook/main.js
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+const path = require("path");
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 // .storybook/preview.js
-import "../../styles/globals.css";
+// import "styles/globals.css";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
